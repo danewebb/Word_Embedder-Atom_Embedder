3be05d0fa1353a13ef8b8af4512bd7cba11af82c
danewebb <56095658+danewebb@users.noreply.github.com>
2020-09-17 20:18:18 -0700

    Add files via upload

---
+++ Process_sciart.py
@@ -0,0 +1,137 @@
+import re
+import time
+import pickle
+import os
+import numpy as np
+import nltk
+from nltk.corpus import stopwords
+
+
+class Process_Sciart():
+
+    def __init__(self, data_list, out_datafile_name, vocabfile=0):
+        """
+        :param datafile: File location of data. Assume no labels
+        :param vocabfile: list of ranked vocab w/ most common in index 1 and 0 in index 0
+        """
+
+
+        self.data_list = data_list
+        self.out_datafile_name = out_datafile_name
+
+        if os.path.exists(r'C:\Users\liqui\PycharmProjects\THESIS\venv\Lib\sciart_wordembedding\sciart_vocab.pkl') and vocabfile==0:
+            # if we want to use the saved clean vocab file
+            with open(r'C:\Users\liqui\PycharmProjects\THESIS\venv\Lib\sciart_wordembedding\sciart_vocab.pkl', 'rb') as voc_file:
+                self.vocab = pickle.load(voc_file)
+        elif vocabfile != 0:
+            # if we want to use a different dirty vocab file
+            self.vocab_list = open(vocabfile, 'rb')
+            self.vocab = self.clean_vocab()
+        else:
+            # if we don't have a vocab file and want to use and save the one from scientific articles tfds on the machine of DANE WEBB
+            self.vocab_list = open(r'C:\Users\liqui\tensorflow_datasets\downloads\extracted\ZIP.ucid_1b3rmCSIoh6VhD4H-cSwcwbeC_export_downloadwN6uevfZyH8l3632IfcSb3CNfcrG01PHVkiDCEoAAHY\arxiv-dataset\vocab.txt',
+                            'r', encoding='utf8')
+            self.vocab = self.clean_vocab()
+            with open('sciart_wordembedding\sciart_vocab.pkl', 'wb') as file:
+                pickle.dump(self.vocab, file)
+
+
+
+    def encode_data(self):
+        pattern = r'\w+'
+        # strip_pattern = r'[^\w]' # not letters
+        # replacement = ''
+
+        art_store = []
+        encoded_articles = []
+
+        datalen = len(self.data_list)
+        report_every = 200
+        stops = set(stopwords.words('english'))  # set of stopwords from nltk
+        for num, line in enumerate(self.data_list):
+            if num % report_every == 0:
+                print(f"Article {num} of {datalen}") # finished w/ article # of ___
+                print(time.asctime(time.localtime(time.time()))) # print time to get eta
+            sep_words = re.findall(pattern, line) #seperate words from string into list of strings. Only takes words w/o symbols or numbers
+            for word in sep_words:
+                if word in self.vocab and word not in stops:
+                    # only store words that are in vocab and are not a stopword
+                    art_store.append(self.vocab.index(word))
+                else:
+                    art_store.append(0)
+
+            encoded_articles.append(art_store)
+            art_store = []
+
+        return encoded_articles
+
+    def clean_vocab(self):
+        # clean numbers off vocab from the raw data
+        vocab_sin_freq = []
+        vocab_only_letters = [0]
+        charstore = []
+        pattern = r'[a-z]+$'
+        for line in self.vocab_list:
+            for char in line:
+                if char == ' ':
+                    vocab_sin_freq.append(''.join(charstore))
+                    charstore = []
+                    break
+                else:
+                    charstore.append(char)
+
+        # eliminate words with symbols and numbers in them
+        for line in vocab_sin_freq:
+            if re.match(pattern, line):
+                vocab_only_letters.append(line)
+
+        # eliminate stopwords ?????
+
+
+
+        return vocab_only_letters
+
+
+
+    def main(self):
+        encoded_articles = self.encode_data()
+
+        with open(self.out_datafile_name, 'wb') as file:
+            pickle.dump(encoded_articles, file)
+
+
+
+
+
+if __name__ == '__main__':
+    """
+    need to find a good way to split up the dataset. Could use
+    data1 = total_data_file.readlines()[0:0.1*datalen]
+    but it is inefficient. 
+    """
+
+    data_name = 'sciart_wordembedding\sciart_data_12.pkl'
+
+    vocab_file = open(r'C:\Users\liqui\tensorflow_datasets\downloads\extracted\ZIP.ucid_1b3rmCSIoh6VhD4H-cSwcwbeC_export_downloadwN6uevfZyH8l3632IfcSb3CNfcrG01PHVkiDCEoAAHY\arxiv-dataset\vocab.txt',
+        'r', encoding='utf8')
+
+    total_data_file = open(r'C:\Users\liqui\tensorflow_datasets\downloads\extracted\ZIP.ucid_1b3rmCSIoh6VhD4H-cSwcwbeC_export_downloadwN6uevfZyH8l3632IfcSb3CNfcrG01PHVkiDCEoAAHY\arxiv-dataset\train.txt',
+        'r')
+    total_rawdata = total_data_file.readlines()
+    datalen = len(total_rawdata)
+
+    split_into = 20 #number of different datafiles
+    split_num = round(datalen/split_into)
+
+
+    ###
+
+    data_want = total_rawdata[11*split_num+1:12*split_num]
+
+    ###
+
+    PSA = Process_Sciart(data_want, data_name)
+    PSA.main()
+
+
+
---
+++ word_embedding.py
@@ -0,0 +1,175 @@
+import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # info and warning messages aren't printed
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers
+import re
+import time
+import pickle
+# import tensorflow_datasets as tfds
+
+import Process_sciart as PSA
+
+# tfds.disable_progress_bar()
+
+
+class Sciart_Word_Embedding():
+
+    def __init__(self, data_chunks, model_path='', labels=0, vocab=0, vocab_size=0, batch_size=20, set_epochs=1, embedding_dim=16,
+                 dense1_size=16, dense2_size=1):
+        """
+
+        :param data_chunks: List of data file paths. Break up data so memory doesn't overflow.
+        :param model: First time through leave blank but once model is save in path, change zero to model path.
+        :param vocab_size: If we are using scientific papers vocab built in Process_sciart, leave vocab_size=0
+        :param labels: Dummy labels = 0, every label is a zero.
+        :param batch_size:
+        :param set_epochs:
+        :param embedding_dim:
+        :param dense1_size: Size of first Dense, 'relu', layer
+        :param dense2_size: Size of second Dense 'default', layer
+        """
+
+
+        self.data_chunks = data_chunks
+        if vocab_size == 0:
+            # get vocab_size from 'sciart_vocab.pkl'
+            self.vocab, self.vocab_size = self.voc()
+        else:
+            self.vocab = vocab
+            self.vocab_size = len(self.vocab)
+
+        self.labels = labels
+
+        self.batch_size = batch_size
+        self.set_epochs = set_epochs
+        self.embedding_dim = embedding_dim
+        self.dense1_size = dense1_size
+        self.dense2_size = dense2_size
+
+
+        if model_path == '':
+            try:
+                # saved model
+                os.path.exists(r'C:\Users\liqui\PycharmProjects\THESIS\venv\Lib\sciart_model')
+                self.model = keras.models.load_model(r'C:\Users\liqui\PycharmProjects\THESIS\venv\Lib\sciart_wordembedding\sciart_model')
+            except:
+                # build new model
+                self.model = self.build_model()
+        else:
+            # load different model
+            self.model = keras.models.load_model(model_path)
+
+    def retrieve_word_embeddings(self, model):
+        ### Retrieve Word Embeddings
+
+        model = keras.models.load_model(model)
+
+        e = model.layers[0]
+        weights = e.get_weights()[0]
+        print(weights.shape)
+        #
+        import io
+        out_v = io.open(r'sciart_wordembedding\vecs.tsv', 'w', encoding='utf-8')
+        out_m = io.open(r'sciart_wordembedding\meta.tsv', 'w', encoding='utf-8')
+        #
+        for num, word in enumerate(self.vocab[1:], start=1):
+            vec = weights[num]
+            out_m.write(word + "\n")
+            out_v.write('\t'.join([str(x) for x in vec]) + "\n")
+
+        out_v.close()
+        out_m.close()
+
+
+    def data_to_numpy(self, data):
+        # make encoded data into numpy arrays
+        train_data_arr = np.array([np.array(x) for x in data])
+
+        padded_train_data = keras.preprocessing.sequence.pad_sequences(
+            train_data_arr, padding='post'
+        )
+
+        tdata_size = padded_train_data.shape
+
+        return padded_train_data, tdata_size
+
+
+
+    def build_model(self):
+        model = keras.Sequential([
+            layers.Embedding(self.vocab_size, self.embedding_dim),
+            layers.GlobalAveragePooling1D(),
+            layers.Dense(self.dense1_size, activation='relu'),
+            layers.Dense(self.dense2_size)
+        ])
+
+        model.compile(
+            optimizer='adam',
+            loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+            metrics=['accuracy']
+        )
+
+        return model
+
+    def train(self, trdata, trlabels):
+        history = model.fit(
+            trdata,
+            trlabels,
+            epochs=self.set_epochs,
+            batch_size=self.batch_size,
+            shuffle=True
+        )
+
+    def voc(self):
+        with open(r'sciart_wordembedding\sciart_vocab.pkl', 'rb') as voc_file:
+            vocab = pickle.load(voc_file)
+        vocab_size = len(vocab)
+        return vocab, vocab_size
+
+    def main(self):
+
+        for chunk in self.data_chunks:
+            with open(chunk, 'rb') as chunk_file:
+                data = pickle.load(chunk_file)
+            trdata, tdata_size = self.data_to_numpy(data)
+            if self.labels == 0:
+                trlabels = np.zeros([tdata_size[0], 1])
+                trlabels = trlabels.astype('int32')
+            else:
+                trlabels = self.labels
+
+        tf.keras.models.save_model(self.model, r'C:\Users\liqui\PycharmProjects\THESIS\venv\Lib\sciart_wordembedding\sciart_model')
+
+
+
+if __name__ == '__main__':
+
+    epochs = 60
+    report_every = 5
+    data_chunks = [r'sciart_wordembedding\sciart_data_01.pkl', r'sciart_wordembedding\sciart_data_02.pkl', r'sciart_wordembedding\sciart_data_03.pkl',
+                   r'sciart_wordembedding\sciart_data_04.pkl', r'sciart_wordembedding\sciart_data_05.pkl', r'sciart_wordembedding\sciart_data_06.pkl',
+                   r'sciart_wordembedding\sciart_data_07.pkl', r'sciart_wordembedding\sciart_data_08.pkl', r'sciart_wordembedding\sciart_data_09.pkl',
+                   r'sciart_wordembedding\sciart_data_10.pkl', r'sciart_wordembedding\sciart_data_11.pkl', r'sciart_wordembedding\sciart_data_12.pkl']
+
+    SWE = Sciart_Word_Embedding(data_chunks)
+
+    start_time = time.time()
+    for ii in range(1, epochs+1):
+        SWE.main()
+        if ii%report_every == 0:
+            end_time = time.time()
+            print(f'{ii}/{epochs} completed')
+
+            elapsed_time = end_time - start_time
+            reports_left = (epochs - ii)/report_every
+            eta = elapsed_time*reports_left/3600 ### Hours
+            print(f'Estimated time to completion: {eta} minutes')
+            start_time = end_time
+
+
+    # SWE.retrieve_word_embeddings(r'sciart_wordembedding\sciart_model')
+
+
