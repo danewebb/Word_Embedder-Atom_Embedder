71c6dc643121c61848fb93c7492e546706829974
danewebb <56095658+danewebb@users.noreply.github.com>
2020-09-25 15:58:08 -0700

    added sum_of_ndelta function

--- atom_embedding.py
+++ atom_embedding.py
@@ -27,7 +27,13 @@ class Atom_Embedder:
             self.encoded_vecs[num] = self.weights[num]
 
 
+    def __init_diffs(self, atom):
+        diffs = []
+        for ii in range(0, len(atom) - 1):
+            # self.encoded_vecs: keys=encoded word, value=word embed vector
+            diffs.append(self.encoded_vecs[atom[ii + 1]] - self.encoded_vecs[atom[ii]])
 
+        return diffs
 
     def sum_of_difference(self, atom):
         """
@@ -37,16 +43,36 @@ class Atom_Embedder:
         ((4-1), (5-2), (6-3)) + ((7-4), (8-5), (9-6))
         """
 
-        diffs = []
-        for ii in range(0, len(atom) - 1):
-            # self.encoded_vecs: keys=encoded word, value=word embed vector
-            diffs.append(self.encoded_vecs[atom[ii+1]] - self.encoded_vecs[atom[ii]])
+        diffs = self.__init_diffs(atom)
 
         # sums all resultant vectors and collapses list
         res = [sum(jj) for jj in zip(*diffs)]
 
         return res
 
+    def sum_of_ndelta(self, atom, n):
+        """
+        n = 1: difference between each vector than sum
+        n = 2: difference between each vector then a difference between resultant then sum.
+        n = 3, 4, 5, ...
+        :param atom:
+        :param n: Number of delta dimensions
+        :return:
+        """
+        diff1 = []
+        diff2 = self.__init_diffs(atom)
+        L = len(diff2)
+        n -= 1
+        while n > 0 and L > 1:
+            for ii in range(0, L-1):
+                diff1.append(diff2[ii+1] - diff2[ii])
+            n -= 1
+            L = len(diff1)
+            diff2 = diff1
+            diff1 = []
+
+        res = [sum(jj) for jj in zip(*diff2)]
+        return res
 
 
     def sum_atoms(self, atom):
@@ -67,6 +93,7 @@ class Atom_Embedder:
 
 if __name__ == '__main__':
     atom_vecs = []
+
     with open(r'C:\Users\liqui\PycharmProjects\Word_Embeddings\Lib\ricocorpus_wordembedding\ranked_vocab.pkl', 'rb') as voc_file:
         vocab = pickle.load(voc_file)
 
@@ -79,7 +106,8 @@ if __name__ == '__main__':
 
     for para in train_vectors:
         # atom_vecs.append(AE.sum_of_difference(para))
-        atom_vecs.append(AE.sum_atoms(para))
+        atom_vecs.append(AE.sum_of_ndelta(para, 1))
+
+
     with open(r'C:\Users\liqui\PycharmProjects\Word_Embeddings\Lib\atom_vectors', 'wb') as file:
         pickle.dump(atom_vecs, file)
-
